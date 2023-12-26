8bfd2ab2b4e0b702dd6750f035dd8d5b96769584
Turg0n <turgon1661@gmail.com>
2023-12-26 18:58:42 +0200

    1-2task

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,24 @@
-function getShippingMessage(country, price, deliveryFee) {
-    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
-}
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+class Storage {
+    #items;
+    constructor (item){
+        this.#items = item; 
+    };
+    getItems(){
+        return this.#items;
+    };
+    
+    addItem(newItem){
+        this.#items.push(newItem);
+        };
+    removeItem(itemToRemove){
+        const indexToRemove = this.#items.indexOf(itemToRemove);
+        this.#items.splice(indexToRemove, 1);
+    };
+};
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
